7d4918d60498d35422bb7c2b716508dce813ce9a
jpskgc <jpskgc.v3@gmail.com>
2020-12-08 03:07:36 +0900

    first commit

---
+++ abc086/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a * b % 2)
+    {
+        cout << "Even" << endl;
+    }
+    else
+    {
+        cout << "Odd" << endl;
+    }
+}
