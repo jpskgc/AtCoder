096a102a7205bd6f23856c52b59ade8d4e7faf0b
jpskgc <jpskgc.v3@gmail.com>
2020-12-08 03:37:39 +0900

    add abc064 a

--- abc064/a.cpp
+++ abc064/a.cpp
@@ -6,8 +6,12 @@ int main()
     int r, g, b;
     cin >> r >> g >> b;
 
-    if ((r * 100 + g * 10 + b) %)
+    if ((r * 100 + g * 10 + b) % 4)
     {
-        cout << "Even" << endl;
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
     }
 }
