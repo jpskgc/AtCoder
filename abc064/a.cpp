89861757cfc252a290db20bd2c8e2e2442a39382
jpskgc <jpskgc.v3@gmail.com>
2020-12-08 03:16:32 +0900

    add abc064 a

---
+++ abc064/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int r, g, b;
+    cin >> r >> g >> b;
+
+    if ((r * 100 + g * 10 + b) %)
+    {
+        cout << "Even" << endl;
+    }
+}
