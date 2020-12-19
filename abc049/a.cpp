e98671231731604981463d6324b74f762fdc2cd9
jpskgc <jpskgc.v3@gmail.com>
2020-12-19 16:55:14 +0900

    [add] abc049 a

---
+++ abc049/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+
+    if (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o') {
+        puts("vowel");
+        return 0;
+    }
+    puts("consonant");
+}
