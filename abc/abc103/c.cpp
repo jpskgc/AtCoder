a2195ed301fae0897471affda67a868f10c53ace
jpskgc <jpskgc.v3@gmail.com>
2021-02-04 23:12:08 +0900

    [add] abc103 c

---
+++ abc/abc103/c.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    long long ans = 0;
+    for (int i = 0; i < N; ++i) {
+        long long a;
+        cin >> a;
+        ans += (a - 1);
+    }
+
+    cout << ans << endl;
+}
