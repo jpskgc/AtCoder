898c78b23f38cd4f345e4ef5347f97d16551145f
jpskgc <jpskgc.v3@gmail.com>
2021-02-28 06:18:23 +0900

    [add] abc181 a

---
+++ abc/abc181/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N % 2 == 0) {
+        puts("White");
+        return 0;
+    }
+
+    puts("Black");
+    return 0;
+}
