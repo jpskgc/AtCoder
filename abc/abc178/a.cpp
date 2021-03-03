11224d37045a72fc1468963df3e167aa58f11a62
jpskgc <jpskgc.v3@gmail.com>
2021-03-03 22:36:01 +0900

    [add] abc178 a

---
+++ abc/abc178/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X == 0) {
+        puts("1");
+        return 0;
+    }
+
+    puts("0");
+    return 0;
+}
